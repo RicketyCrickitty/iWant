b4890c6f24def30c3cd74cf678a7a14cbc73960d
ricketycrickity <ricjames@vt.edu>
2023-11-07 15:42:57 -0500

    Adding Create React App files

---
+++ iwant/src/App.js
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="iWant">
+      <h1>iWant - A Wishlist Website</h1>
+    </div>  
+  );
+}
+
+export default App;
---
+++ iwant/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const iWantHeader = <h1>iWant - A Wishlist Website</h1>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(iWantHeader);  
